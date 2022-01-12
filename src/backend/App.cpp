357dbb07f07277aa991a03ac15b572e908761076
OopsOverflow <houssemmahmoudswe@gmail.com>
2022-01-12 19:04:16 +0100

    Add SFML

--- src/backend/App.cpp
+++ src/backend/App.cpp
@@ -8,8 +8,30 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include <SFML/Graphics.hpp>
+
 
 int main(){
+
+        sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Blue);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
+
+        return 0;
 
 //
 //    srand(time(nullptr));
